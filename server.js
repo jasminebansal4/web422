675e566f45812abc92ac9c5b5b77b76961524383
jasminebansal4 <jasminebansal4@gmail.com>
2025-01-21 11:22:02 -0500

    Changes

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 1
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Jasmine__________ Student ID: 101594232_______ Date: 21-01-2025___________
+* Vercel Link: _______________________________________________________________
+*
+********************************************************************************/
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
